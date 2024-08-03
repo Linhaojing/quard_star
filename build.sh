bdc72e6a09eb9ddb8839404e0e52d1dd0049febb
linhaojing <linhaojing@huawei.com>
2024-08-03 16:22:12 +0800

    Support opensbi
    Signed-off-by: linhaojing <linhaojing@huawei.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 cwd=$(pwd)
+CROSS_PREFIX=riscv64-linux-gnu-
 
 cd qemu/build
 
@@ -17,5 +18,24 @@ cd ${cwd}/lowlevelboot
 make clean
 make
 mkdir -p ${cwd}/output/lowlevelboot
-cp boot.bin ${cwd}/output/lowlevelboot
+rm -rf ${cwd}/output/lowlevelboot/*
+cp lowlevel_boot.bin ${cwd}/output/lowlevelboot
+cd -
+
+
+mkdir -p ${cwd}/output/opensbi
+rm -rf ${cwd}/output/opensbi/*
+cd ${cwd}/opensbi
+make CROSS_COMPILE=$CROSS_PREFIX PLATFORM=quard_star
+cp -r ${cwd}/opensbi/build/platform/quard_star/firmware/*.bin ${cwd}/output/opensbi/
+cd -
+
+dtc -I dts -O dtb -o ${cwd}/output/opensbi/quard_star_sbi.dtb ${cwd}/dts/quard_star_sbi.dts
+
+cd ${cwd}/output
+rm -rf boot.bin
+dd of=boot.bin bs=1k count=32k if=/dev/zero
+dd of=boot.bin bs=1k conv=notrunc seek=0 if=lowlevelboot/lowlevel_boot.bin
+dd of=boot.bin bs=1k conv=notrunc seek=512 if=opensbi/quard_star_sbi.dtb
+dd of=boot.bin bs=1k conv=notrunc seek=2k if=opensbi/fw_jump.bin
 cd -
--- run.sh
+++ run.sh
@@ -6,5 +6,9 @@ ${cwd}/output/qemu/bin/qemu-system-riscv64 \
 -M quard-star \
 -m 1G \
 -smp 8 \
--drive if=pflash,bus=0,unit=0,format=raw,file=${cwd}/output/lowlevelboot/boot.bin \
--nographic --parallel none
+-drive if=pflash,bus=0,unit=0,format=raw,file=${cwd}/output/boot.bin \
+--parallel none \
+-nographic \
+# -S -s
+
+# -nographic
